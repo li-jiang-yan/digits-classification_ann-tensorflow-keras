edc9094c5c39f33056c2514eb7bd3f2cef9c2f7b
li-jiang-yan <professional.jiangyan_li@outlook.com>
2025-10-20 16:05:44 +0800

    Added splitting of training and testing data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 # Load dataset
 digits = datasets.load_digits()
@@ -6,3 +7,6 @@ A = digits.data
 b = digits.target
 num_classes = len(digits.target_names)
 num_features = len(digits.feature_names)
+
+# Split training/testing data
+A_train, A_test, b_train, b_test = train_test_split(A, b, random_state=1)
